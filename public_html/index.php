00c4cb7342189873ee36f2954ab34d40a13b6b26
Tobias Lindberg <tobias.ehlert@gmail.com>
2022-07-21 09:57:22 +0200

    update Google Analytics code to use gtag with GA4

--- public_html/index.php
+++ public_html/index.php
@@ -54,14 +54,14 @@ $app->get( '/', function($request, $response, $args) use ($config)
             <script src="js/html5shiv.js"></script>
             <script src="js/respond.min.js"></script>
         <![endif]-->
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            ga('create', '<?php echo $config['lobbymirror']['analytics']; ?>', 'auto');
-            ga('send', 'pageview');
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $config['lobbymirror']['analytics']; ?>"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '<?php echo $config['lobbymirror']['analytics']; ?>');
         </script>
     </head>
     <body>
